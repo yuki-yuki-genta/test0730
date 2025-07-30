47cb0c9ca4b2a4f367d840c380f96252075f4da6
yuki-yuki-genta <yuki.kanno@capgemini.com>
2025-07-30 08:45:00 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+app.get('/', (req, res) => {
+  res.send('Hello from ECS Web App!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
